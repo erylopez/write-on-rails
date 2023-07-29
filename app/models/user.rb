510ff224ff62772f56c6cdc9e8d9b009d60fe6eb
Matt Hinczuk <matiasmoya@gmail.com>
2023-07-29 02:49:36 -0300

    Adds devise + omniauth for github authentication. Saves authorization token from github in the user record

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ApplicationRecord
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :omniauthable,
+         omniauth_providers: [:github]
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0, 20]
+      user.github_access_token = auth.credentials.token
+    end
+  end
+end
