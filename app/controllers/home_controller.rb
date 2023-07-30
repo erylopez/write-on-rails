f7c6a89999845e34628511403bbcbbc3a670da29
Matt Hinczuk <matiasmoya@gmail.com>
2023-07-30 19:59:14 -0300

    adds post card partial to reuse it in post#index

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
   def index
     redirect_to onboarding_index_path and return if current_user && current_user.onboarding_step != "step_4"
+    redirect_to dashboard_index_path and return if current_user && current_user.onboarding_step == "step_4"
   end
 
   def login
