e26497504b3bf22442c0f2dc3f116d9dd92ec33e
Erika Lopez <lopez1erika@gmail.com>
2023-08-07 11:03:07 -0300

    wip posts#show and sidebar components

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,9 @@ module ApplicationHelper
   def test
     current_user.attributes_from_keys(:dev_to_api_key, :hashnode_access_token)
   end
+
+  def render_badge(text:, color:)
+    tailwind_classes = "bg-#{color}-100 text-#{color}-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-#{color}-400 border border-#{color}-400"
+    "<span class='#{tailwind_classes}'>#{text}</span>".html_safe
+  end
 end
