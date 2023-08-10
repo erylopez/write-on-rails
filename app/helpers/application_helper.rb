9d6319dbdafed9e444d598de1c6fae75a9851d52
Erika Lopez <lopez1erika@gmail.com>
2023-08-09 21:55:22 -0300

    Post#show basic frontend

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,7 +9,7 @@ module ApplicationHelper
   end
 
   def render_badge(text:, color:)
-    tailwind_classes = "bg-#{color}-100 text-#{color}-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-#{color}-400 border border-#{color}-400"
+    tailwind_classes = "bg-#{color}-200/50 text-#{color}-700 text-sm font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-#{color}-700/50 dark:text-#{color}-200 border border-#{color}-400"
     "<span class='#{tailwind_classes}'>#{text}</span>".html_safe
   end
 end
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,5 +13,18 @@ module.exports = {
     require('@tailwindcss/forms'),
     require('@tailwindcss/aspect-ratio'),
     require('@tailwindcss/line-clamp'),
-  ]
+  ],
+  safelist: [
+    'bg-green-200/50',
+    'text-green-700',
+    'dark:bg-green-700/50',
+    'dark:text-green-200',
+    'border-green-400',
+    'bg-red-200/50',
+    'text-red-700',
+    'dark:bg-red-700/50',
+    'dark:text-red-200',
+    'border-red-400'
+  ],
+
 }
