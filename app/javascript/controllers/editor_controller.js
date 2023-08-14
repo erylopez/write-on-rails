e8c5fea5e10f90edeb732f6b6dd803906ff1374f
Matt Hinczuk <matiasmoya@gmail.com>
2023-08-14 19:12:02 -0300

    experimental markdown editor using toast-ui-editor

---
+++ app/javascript/controllers/editor_controller.js
@@ -0,0 +1,35 @@
+import { Controller } from "@hotwired/stimulus"
+import Prism from 'prismjs';
+import '../prism-languages';
+import codeSyntaxHighlight from '@toast-ui/editor-plugin-code-syntax-highlight';
+import Viewer from '@toast-ui/editor/dist/toastui-editor-viewer';
+import Editor from '@toast-ui/editor';
+
+export default class extends Controller {
+  static targets = ["field"]
+
+  onChange() {
+    console.log(this.editor.getMarkdown())
+  }
+
+  connect() {
+    this.editor = new Editor({
+      el: document.getElementById('editor'),
+      previewStyle: 'vertical',
+      height: '500px',
+      initialEditType: 'markdown',
+      initialValue: '```ruby\ndef something\n  return true\nend\n```',
+      events: {
+        change: this.onChange.bind(this)
+      },
+      plugins: [[codeSyntaxHighlight, { highlighter: Prism }]]
+    });
+
+    const viewer = new Viewer({
+      el: document.querySelector('#viewer'),
+      height: '600px',
+      initialValue: '# Demo\n```ruby\ndef something\n  return true\nend\n```',
+      plugins: [[codeSyntaxHighlight, { highlighter: Prism }]]
+    });
+  }
+}
---
+++ app/javascript/prism-languages.js
@@ -0,0 +1,24 @@
+//supported languages for prismjs highlighter
+import 'prismjs/components/prism-ruby';
+import 'prismjs/components/prism-javascript';
+import 'prismjs/components/prism-css';
+import 'prismjs/components/prism-bash';
+import 'prismjs/components/prism-sql';
+import 'prismjs/components/prism-yaml';
+import 'prismjs/components/prism-json';
+import 'prismjs/components/prism-docker';
+import 'prismjs/components/prism-elixir';
+import 'prismjs/components/prism-go';
+import 'prismjs/components/prism-java';
+import 'prismjs/components/prism-kotlin';
+import 'prismjs/components/prism-python';
+import 'prismjs/components/prism-swift';
+import 'prismjs/components/prism-csharp';
+import 'prismjs/components/prism-typescript';
+import 'prismjs/components/prism-rust';
+import 'prismjs/components/prism-sass';
+import 'prismjs/components/prism-scss';
+import 'prismjs/components/prism-markdown';
+import 'prismjs/components/prism-makefile';
+import 'prismjs/components/prism-javascript';
+import 'prismjs/components/prism-nginx';
