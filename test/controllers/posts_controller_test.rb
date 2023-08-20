7b80324a62e5175c5bca7e7ebc92f76d6183caa6
Matt Hinczuk <matiasmoya@gmail.com>
2023-08-20 17:44:07 -0300

    Adds test for posts_controller#destroy action

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -11,4 +11,15 @@ class PostsControllerTest < ActionDispatch::IntegrationTest
     get posts_path
     assert_response :success
   end
+
+  test "#destroy" do
+    Devto::UpdatePublished.any_instance.stubs(:call).returns(true)
+    user = users(:one)
+    sign_in user
+    post = user.posts.create!(title: "Hello, World!", md_content: "This is a test post.", devto_id: 1)
+
+    assert_difference("Post.count", -1) do
+      delete post_path(post)
+    end
+  end
 end
