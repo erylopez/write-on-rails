cedf941320dda8d6a075a22ef0850900b98a8ccf
Matt Hinczuk <matiasmoya@gmail.com>
2023-08-27 21:40:15 -0300

    Adds hashnode delete service

---
+++ app/services/hashnode/delete_post.rb
@@ -0,0 +1,25 @@
+class Hashnode::DeletePost < Hashnode::Base
+  def initialize(authorization_code:, publication_id:, post:)
+    @authorization_code = authorization_code
+    @publication_id = publication_id
+    @post = post
+  end
+
+  def call
+    query = %{
+      mutation deletePost($id: String!){
+        deletePost(id: $id){
+          code
+          success
+          message
+        }
+      }
+    }.gsub(/\n+/, " ").strip
+
+    variables = {id: @post.hashnode_id}
+
+    headers = {"Content-Type": "application/json", Authorization: @authorization_code}
+
+    HTTParty.post("https://api.hashnode.com", body: {query: query, variables: variables}.to_json, headers: headers)
+  end
+end
---
+++ test/services/hashnode/delete_post_test.rb
@@ -0,0 +1,32 @@
+require "test_helper"
+
+class Hashnode::DeletePostTest < ActiveSupport::TestCase
+  test "deletes a post in hashnode" do
+    VCR.use_cassette("hashnode/delete_post") do
+      post = Post.create(title: "Created in tests", md_content: "Created **in** tests")
+
+      response = Hashnode::CreatePost.new(
+        authorization_code: "my-secret-authorization-code",
+        publication_id: "my-publication-id",
+        post: post
+      ).call
+
+      post.update(
+        hashnode_id: response.dig("data", "createStory", "post", "_id"),
+        hashnode_slug: response.dig("data", "createStory", "post", "slug")
+      )
+
+      assert_equal HTTParty::Response, response.class
+      assert response.dig("data", "createStory", "post", "_id").present?
+
+      delete_response = Hashnode::DeletePost.new(
+        authorization_code: "my-secret-authorization-code",
+        publication_id: "my-publication-id",
+        post: post
+      ).call
+
+      assert_equal HTTParty::Response, delete_response.class
+      assert delete_response.dig("data", "deletePost", "success")
+    end
+  end
+end
