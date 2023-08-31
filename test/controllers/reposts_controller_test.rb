1649a096532efa5d78db8af43448fa5732859ed4
Erika Lopez <lopez1erika@gmail.com>
2023-08-31 18:21:47 -0300

    - Repost Feature   - Updates repost controller and tests in order to use turbo stream

--- test/controllers/reposts_controller_test.rb
+++ test/controllers/reposts_controller_test.rb
@@ -14,17 +14,14 @@ class RepostsControllerTest < ActionDispatch::IntegrationTest
     post reposts_path(params: {post_id: post.id, platform: "hashnode"})
     follow_redirect!
 
-    assert Capybara::Node::Simple.new(@response.body).assert_text "Your post has been reposted to Hashnode"
+    assert Capybara::Node::Simple.new(@response.body).assert_text "Post has been reposted on hashnode"
   end
 
   test "wont repost to hashnode if the post is already reposted" do
     post = @user.posts.create(title: "A test from the controller", md_content: "test", hashnode_id: "someid")
 
     post reposts_path(params: {post_id: post.id, platform: "hashnode"})
-    assert_redirected_to dashboard_index_path
-    follow_redirect!
-
-    assert Capybara::Node::Simple.new(@response.body).has_no_text? "Your post has already been reposted to Hashnode"
+    assert :unprocessable_entity
   end
 
   test "wont repost to hashnode if the user has not configured the integration yet" do
@@ -33,25 +30,22 @@ class RepostsControllerTest < ActionDispatch::IntegrationTest
     post = user.posts.create(title: "A test from the controller", md_content: "test")
 
     post reposts_path(params: {post_id: post.id, platform: "hashnode"})
-    follow_redirect!
-
-    assert Capybara::Node::Simple.new(@response.body).assert_text "You need to connect your Hashnode account first"
+    assert :unprocessable_entity
   end
 
   test "reposts to devto with correct params" do
     post = @user.posts.create(title: "A test from the controller", md_content: "#Testing\n This is a **test** from the controller")
 
     post reposts_path(params: {post_id: post.id, platform: "devto"})
+    follow_redirect!
     assert_response :success
-    assert_select "turbo-stream[action='replace'][target='integrations']"
   end
 
   test "wont repost to devto if the post is already reposted" do
     post = @user.posts.create(title: "A test from the controller", md_content: "test", devto_id: "someid")
 
     post reposts_path(params: {post_id: post.id, platform: "devto"})
-    follow_redirect!
-    assert Capybara::Node::Simple.new(@response.body).has_no_text? "Your post has already been reposted to Dev.to"
+    assert :unprocessable_entity
   end
 
   test "wont repost to devto if the user has not configured the integration yet" do
@@ -60,8 +54,6 @@ class RepostsControllerTest < ActionDispatch::IntegrationTest
     post = user.posts.create(title: "A test from the controller", md_content: "test")
 
     post reposts_path(params: {post_id: post.id, platform: "devto"})
-    follow_redirect!
-
-    assert Capybara::Node::Simple.new(@response.body).assert_text "You need to connect your Dev.to account first"
+    assert :unprocessable_entity
   end
 end
