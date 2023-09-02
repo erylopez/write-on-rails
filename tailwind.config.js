9c6e2b32703556fc05a809a74aae7ef94b8aaeee
Erika Lopez <lopez1erika@gmail.com>
2023-09-02 00:22:36 -0300

    Removes Flowbite

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,9 @@ module.exports = {
     './app/views/**/*.html.erb',
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
-    './app/javascript/**/*.js',
-    "./node_modules/flowbite/**/*.js"
+    './app/javascript/**/*.js'
   ],
   plugins: [
-    require('flowbite/plugin'),
     require('@tailwindcss/typography'),
     require('@tailwindcss/forms'),
     require('@tailwindcss/aspect-ratio'),
